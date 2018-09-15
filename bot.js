b4c5eb13906000543f1a91b52a6a965ca4647cfb
Venoom Network <venoomnetwork@gmail.com>
2018-09-15 16:19:06 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (!message.content === 'hello') {
+    if (message.content === 'hello') {
     	message.reply('Hello there!');
   	}
 });
