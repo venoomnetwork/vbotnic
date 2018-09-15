9ed1dcb731b4a42dea32ebdaf2b389f3ddc0d8d1
Venoom Network <venoomnetwork@gmail.com>
2018-09-15 16:26:24 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log('I am ready!');
+    console.log('ONLINE');
 });
 
 client.on('message', message => {
